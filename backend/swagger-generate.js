aa46db7ae8609c5cf729ba80974f2a98b1617ba6
Centman7089 <innocentimitini@gmail.com>
2025-06-19 18:08:54 +0100

    committeds

--- backend/swagger-generate.js
+++ backend/swagger-generate.js
@@ -2,12 +2,26 @@
 // import api from "../routes/userRoutes.js"
 import swaggerAutogen from "swagger-autogen"
 
+const doc = {
+    info: {
+      title: 'InnoTech API',
+      description: 'API documentation for the InnoTech platform',
+      version: '1.0.0',
+    },
+    servers: [
+      {
+        url: 'https://page-inno-tech.onrender.com',
+        description: 'Production Render Server'
+      }
+    ],
+  };
+
 const outputFile = "./swagger-output.json"; // output file
 
 // const endpointsFiles = "../routes/userRoutes.js" 
 const endpointsFiles = [ "./server.js" ] //
 
-swaggerAutogen( outputFile, endpointsFiles ).then( () => (
+swaggerAutogen( outputFile, endpointsFiles,doc ).then( () => (
     console.log("swagger documentation generated")
     
     
