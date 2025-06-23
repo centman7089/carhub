18c8843b2c612d7e9d15f8d6dbca21f8426eeebd
Centman7089 <innocentimitini@gmail.com>
2025-06-23 11:53:07 +0100

    update 2

--- backend/controllers/employerController.js
+++ backend/controllers/employerController.js
@@ -92,7 +92,7 @@ const register = async ( req, res ) =>
 const login = async (req, res) => {
 	try {
 		const { email, password } = req.body;
-		const user = await User.findOne( { email} );
+		const user = await Employer.findOne( { email} );
 		if (!user || !user.isVerified) return res.status(400).json({ msg: "Invalid credentials or unverified email" });
 		const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
 
@@ -115,35 +115,7 @@ const login = async (req, res) => {
 		res.status(500).json({ error: error.message });
 		console.log("Error in loginUser: ", error.message);
 	}
-};	try {
-		const { email, password } = req.body;
-		const user = await Employer.findOne( { email} ).select('-password');
-		if (!user || !user.isVerified) return res.status(400).json({ msg: "Invalid credentials or unverified email" });
-		const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
 
-		if (!isPasswordCorrect) return res.status(400).json({ error: "Invalid email or password" });
-
-		const token = generateTokenAndSetCookie(user._id, res);
-
-		res.status( 200 ).json( {
-			token,
-			_id: user._id,
-			email: user.email,
-		});
-	} catch (error) {
-		res.status(500).json({ error: error.message });
-		console.log("Error in loginUser: ", error.message);
-	}
-};
-
-const logoutUser = (req, res) => {
-	try {
-		res.cookie("jwt", "", { maxAge: 1 });
-		res.status(200).json({ message: "User logged out successfully" });
-	} catch (err) {
-		res.status(500).json({ error: err.message });
-		console.log("Error in signupUser: ", err.message);
-	}
 };
 
 
@@ -347,7 +319,15 @@ const forgotPassword = async (req, res) => {
 	}
   };
 
-  
+  const logoutUser = (req, res) => {
+	try {
+		res.cookie("jwt", "", { maxAge: 1 });
+		res.status(200).json({ message: "User logged out successfully" });
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+		console.log("Error in signupUser: ", err.message);
+	}
+};
 
 export
 {
