aa981849672e1302deb261c0dfc2f2e91d228ea8
Centman7089 <innocentimitini@gmail.com>
2025-07-09 08:46:21 +0100

    committed

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -201,19 +201,18 @@ const getFeedPosts = async (req, res) => {
 };
 
 const getUserPosts = async (req, res) => {
-	const { id } = req.params;
-	try {
-		const user = await User.findOne({ id });
-		if (!user) {
-			return res.status(404).json({ error: "User not found" });
-		}
+    const { id } = req.params;
+    try {
+        const user = await User.findById(id);
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
 
-		const posts = await Post.find({ postedBy: user._id }).sort({ createdAt: -1 });
-
-		res.status(200).json(posts);
-	} catch (error) {
-		res.status(500).json({ error: error.message });
-	}
+        const posts = await Post.find({ postedBy: user._id }).sort({ createdAt: -1 });
+        res.status(200).json(posts);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 };
 
 export { createPost, getPost, deletePost, likeUnlikePost, replyToPost, getFeedPosts, getUserPosts, updatePost };
