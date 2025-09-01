73fdb6b8233d7d98712771bf3ebdc0bdc2e7e981
Centman7089 <innocentimitini@gmail.com>
2025-09-01 15:55:13 +0100

    updated

--- backend/controllers/adminControllers.js
+++ backend/controllers/adminControllers.js
@@ -412,9 +412,10 @@ export const getAdminUser = async (req, res) => {
 
 export const approveUser = async (req, res) => {
   try {
-    const { userId } = req.params;
+    // const { userId } = req.params;
+    const userId = req.user._id
 
-    const user = await User.findById(userId);
+    const user = await User.findById(req.params.userId);
     if (!user) return res.status(404).json({ error: "User not found" });
 
     user.identityDocuments.status = "approved";
