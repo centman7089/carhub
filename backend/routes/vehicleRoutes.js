6aea96edbb0a7b83319e5a89e02eefb6fcf001d1
Centman7089 <innocentimitini@gmail.com>
2025-09-02 15:50:48 +0100

    updated

--- backend/routes/vehicleRoutes.js
+++ backend/routes/vehicleRoutes.js
@@ -9,10 +9,11 @@ import {
   deleteVehicle,
 } from '../controllers/vehicleController.js';
 import { vehicleImages } from '../middlewares/upload.js';
+import { protectAdmin } from '../middlewares/adminAuth.js';
 
 const router = express.Router();
 
-router.post('/create-vehicle', vehicleImages,createVehicle);
+router.post('/create-vehicle', protectAdmin,vehicleImages,createVehicle);
 router.get('/', getVehicles);
 router.get('/:id', getVehicleById);
 router.put('/:id', upload.array('images', 5), updateVehicle);
