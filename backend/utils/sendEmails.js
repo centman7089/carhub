abf77e9e68c978ee63a8120ca43b5b0c45a5018e
Centman7089 <innocentimitini@gmail.com>
2025-09-25 13:45:26 +0100

    update

--- backend/utils/sendEmails.js
+++ backend/utils/sendEmails.js
@@ -160,6 +160,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  debug: true
 });
 
 // ✅ Verify transporter on startup
